a091b2f386466415b7d2df08a506195bf28a9ca8
Ayxan Abdullayev <ayxanozal5@gmail.com>
2023-11-19 16:30:14 +0400

    Oglanam

---
+++ T2.js
@@ -0,0 +1,50 @@
+let first = document.querySelector('.first')
+let second = document.querySelector('.second')
+let third = document.querySelector('.third')
+let fourth = document.querySelector('.fourth')
+let fifth = document.querySelector('.fifth')
+let bruh = document.querySelectorAll('.sections')
+first.addEventListener('click',()=>{
+    bruh.forEach(element => {
+        if(element.classList.contains('sections')){
+            element.style.display = 'inline-block'
+        }
+    }) 
+})
+second.addEventListener('click',()=>{
+    bruh.forEach(element => {
+        if(element.classList.contains('pack')){
+            element.style.display = 'inline-block'
+        }else{
+            element.style.display = 'none'
+        }
+    })        
+})
+third.addEventListener('click',()=>{
+    bruh.forEach(element => {
+        if(element.classList.contains('mockup')){
+            element.style.display = 'inline-block'
+        }else{
+            element.style.display = 'none'
+        }
+    })        
+})
+fourth.addEventListener('click',()=>{
+    bruh.forEach(element => {
+        if(element.classList.contains('typo')){
+            element.style.display = 'inline-block'
+        }else{
+            element.style.display = 'none'
+        }
+    })        
+})
+fifth.addEventListener('click',()=>{
+    bruh.forEach(element => {
+        if(element.classList.contains('pho')){
+            element.style.display = 'inline-block'
+        }else{
+            element.style.display = 'none'
+        }
+    })        
+})
+    
